09ff6c6cf4e509badaeae77bb36af613d8dfbe55
AngelGabrielTG <angeltg1307@gmail.com>
2020-06-10 22:51:13 -0500

    Agregando AXIOS Y listando los Clientes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //COMPONENTES
@@ -6,14 +6,34 @@ import Clientes from './components/Clientes';
 import NuevoCliente from './components/NuevoCliente';
 import Cliente from './components/Cliente';
 
+import clienteAxios from './config/axios';
+
 function App() {
+
+  //STATE DE LA APP
+  const [listado, guardarCliente] = useState([]);
+
+  useEffect( () => {
+    const consultarAPI = () => {
+      clienteAxios.get('/api/clientes')
+        .then(respuesta => {
+          //COLOCAR EN EL STATE EL RESULTADO
+          guardarCliente(respuesta.data);
+        })
+        .catch(error => {
+          console.log(error)
+        })  
+    }
+    consultarAPI();
+  }, [] );
+
   return (
     <Router>
       <Switch>
         <Route
           exact
           path="/"
-          component={Clientes}
+          component={() => <Clientes listado={listado}/>}
         />
 
         <Route
--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
-const Clientes = () => {
+const Clientes = ({listado}) => {
+    if(listado.length === 0) return null;
     return ( 
-        <h1>Desde Clientes</h1>
-
+        <Fragment>
+            <section className="container">
+                <div className="row btn-registro">
+                    <Link to={'/nuevo'} className="waves-effect waves-light btn"><i className="material-icons right">input</i>REGISTRAR</Link>
+                </div>
+                <div className="row">
+                    <table className="highlight centered responsive-table z-depth-1">
+                        <thead>
+                        <tr>
+                            <th>NOMBRE</th>
+                            <th>APELLIDO</th>
+                            <th>EMAIL</th>
+                            <th>FECHA</th>
+                            <th>OPCIONES</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            listado.map(list => (
+                                <tr key={list.id}>
+                                    <td>{list.nombre}</td>
+                                    <td>{list.apellido}</td>
+                                    <td>{list.email}</td>
+                                    <td>{list.createAt}</td>
+                                    <td>
+                                        <div className="btn-group" role="group">
+                                            <button className="btn waves-effect waves-light green" type="submit" name="action">EDITAR
+                                                <i className="material-icons right">edit</i>
+                                            </button>
+                                            <button className="btn waves-effect waves-light red" type="submit" name="action">ELIMINAR
+                                                <i className="material-icons right">delete</i>
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                        </tbody>
+                    </table>    
+                </div>
+            </section>
+        </Fragment>
     );
 }
  
